57417e7456bdd055f35fa42f403b3fb1b480c368
zhyuanyuan <zhyyituse@163.com>
2015-12-27 17:53:09 +0800

    完成了头部导航

--- App/scripts/configframe/modules/indexModule.js
+++ App/scripts/configframe/modules/indexModule.js
@@ -2,5 +2,6 @@
  * Created by zhangyuanyuan150923 on 2015/12/25.
  */
 define(['angular',"common/modules"],function (angular) {
+    require([ "css!../framworks/font-awesome/font-awesome.min.css","css!../framworks/simple-line-icons/css/simple-line-icons.css"]);
     return angular.module("indexModule",["commonModule"]);
 });
